e10ee0195e55e57e0ac148257467911b17c77f99
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-11-28 14:08:43 +0100

    Grundlage übernommen von Jasper/Flo

---
+++ D5/src/LPS_Niklas_Jordan_SMIB/BinaryPersistency.java
@@ -0,0 +1,21 @@
+package LPS_Niklas_Jordan_SMIB;
+
+import java.io.*;
+
+public class BinaryPersistency implements Persistency {
+
+    @Override
+    public void save(Zettelkasten zk, String filename) throws IOException {
+        ObjectOutput output = new ObjectOutputStream(new FileOutputStream(filename));
+        output.writeObject(zk);
+        output.close();
+    }
+
+    @Override
+    public Zettelkasten load(String filename) throws IOException, ClassNotFoundException {
+        ObjectInput input = new ObjectInputStream(new FileInputStream(filename));
+        Zettelkasten zk = (Zettelkasten) input.readObject();
+        input.close();
+        return zk;
+    }
+}
