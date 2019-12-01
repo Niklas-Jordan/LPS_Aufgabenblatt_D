680caa6909b563443df4a5a83335e824c84ed4de
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-01 01:23:35 +0100

    Aufgabe 5/6 funktionieren, !!!Achtung bei der Eingabe der Titel!!! Aufgabe 7 Class Wikibuch erstellt, jedoch noch unvollständig

--- D5/src/LPS_Niklas_Jordan_SMIB/WebsiteRequest.java
+++ D5/src/LPS_Niklas_Jordan_SMIB/WebsiteRequest.java
@@ -30,7 +30,7 @@ public class WebsiteRequest {
         URL url = new URL("https://de.wikibooks.org/wiki/Spezial:Exportieren/" + title);
         URLConnection connection = url.openConnection();
         // setting user-agent isn't needed, it works also without setting it
-        connection.setRequestProperty("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
+        // connection.setRequestProperty("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3");
         connection.setDoInput(true);
         InputStream inputStream = connection.getInputStream();
         InputSource inputSource = new InputSource(inputStream);
---
+++ D5/src/LPS_Niklas_Jordan_SMIB/WikiBuch.java
@@ -0,0 +1,4 @@
+package LPS_Niklas_Jordan_SMIB;
+
+public class WikiBuch {
+}
